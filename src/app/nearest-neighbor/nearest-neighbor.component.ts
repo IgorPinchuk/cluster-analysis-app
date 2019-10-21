aa1557346d0d1204e17ad544c2ddf63a5daea690
IgorPinchuk <igor_pig@mail.ru>
2019-10-21 10:43:42 +0300

    Кластеры похожи на правду. Добавлена рисовалка

--- src/app/nearest-neighbor/nearest-neighbor.component.ts
+++ src/app/nearest-neighbor/nearest-neighbor.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as CanvasJS from './canvasjs.min';
 
 @Component({
   selector: 'app-nearest-neighbor',
@@ -24,14 +25,6 @@ export class NearestNeighborComponent implements OnInit {
   x3a1 = 8; x3a2= 6;
   x4a1 = 12; x4a2= 11;
   x5a1 = 13; x5a2= 9;
-
-  inputData = [
-    [this.x1a1, this.x1a2],
-    [this.x2a1, this.x2a2],
-    [this.x3a1, this.x3a2],
-    [this.x4a1, this.x4a2],
-    [this.x5a1, this.x5a2]
-  ];
 
   stepObj1 = {
     'distMatrix' : [[],[],[],[],[]],
@@ -77,8 +70,15 @@ export class NearestNeighborComponent implements OnInit {
   calculationsComplete = false;
 
 
-  calcDistanceMatrix(stepObj, arr){
-
+  calcDistanceMatrix(stepObj){
+    this.flushStepObjects();
+    let arr = [
+      [this.x1a1.valueOf(), this.x1a2.valueOf()],
+      [this.x2a1.valueOf(), this.x2a2.valueOf()],
+      [this.x3a1.valueOf(), this.x3a2.valueOf()],
+      [this.x4a1.valueOf(), this.x4a2.valueOf()],
+      [this.x5a1.valueOf(), this.x5a2.valueOf()]
+    ];
     let minI = 0, minJ =0;
     let minVal = Number.MAX_VALUE;
 
@@ -107,6 +107,7 @@ export class NearestNeighborComponent implements OnInit {
     this.addClusterToMatrix(this.stepObj1, this.stepObj2);
     this.addClusterToMatrix(this.stepObj2, this.stepObj3);
     this.addClusterToMatrix(this.stepObj3, this.stepObj4);
+    this.renderChart();
   }
 
   
@@ -122,6 +123,7 @@ export class NearestNeighborComponent implements OnInit {
         for(let j = 0, jj = 0; j<prevStep.distMatrix.length; j++){
           if(j != prevStep.minJ && j != prevStep.minI){
             curStep.distMatrix[ii][jj].value = prevStep.distMatrix[i][j].value;
+            curStep.distMatrix[ii][ii].value = 0;
             jj++;
           } 
         }
@@ -129,16 +131,22 @@ export class NearestNeighborComponent implements OnInit {
         masterCounter ++;
       }
     }
-    for(let i = 0, ii = 0; i<prevStep.distMatrix.length; i++){
-      if(i != prevStep.minI) {
-        if(ii != masterCounter){
-          let value = 0.5 * (( prevMatrix[i][prevMinI].value + prevMatrix[i][prevMinJ].value ) -
-                      (0.5 * Math.abs(prevMatrix[i][prevMinI].value - prevMatrix[i][prevMinJ].value)));  
-          curStep.distMatrix[ii][masterCounter].value = curStep.distMatrix[masterCounter][ii].value = value;
-          ii++;
-        } else {curStep.distMatrix[masterCounter][masterCounter].value = 0}
+    let vector = [];
+    for(let i = 0; i<prevStep.distMatrix.length; i++){
+      if(i !=prevStep.minJ && i !=prevStep.minI){
+        vector.push(Math.min(prevStep.distMatrix[i][prevStep.minJ].value,prevStep.distMatrix[i][prevStep.minI].value));
+      } else {
+        
       }
     }
+
+    for(let i = 0; i<curStep.distMatrix.length; i++){
+      curStep.distMatrix[i][curStep.distMatrix.length-1].value = vector[i];
+      curStep.distMatrix[curStep.distMatrix.length-1][i].value = vector[i];
+      curStep.distMatrix[i][i].value = 0;
+    }
+
+    
     this.findMin(curStep);
     this.generateTitle(prevStep, curStep)
 
@@ -177,6 +185,72 @@ export class NearestNeighborComponent implements OnInit {
       }
     }
     curStep.title.push('('+prevStep.title[prevStep.minI]+', '+prevStep.title[prevStep.minJ]+')')
+  }
+
+  renderChart(){
+    let chart = new CanvasJS.Chart("chartContainer", {
+
+      data: [{
+        type: "scatter",
+        name: "Исходные данные",
+        showInLegend: true,
+        dataPoints: [
+          { x: this.x1a1, y: this.x1a2 },
+          { x: this.x2a1, y: this.x2a2 },
+          { x: this.x3a1, y: this.x3a2 },
+          { x: this.x4a1, y: this.x4a2 },
+          { x: this.x5a1, y: this.x5a2 }
+          
+        ]
+      },
+      {
+        type: "scatter",
+        name: "Центры кластеров",
+        showInLegend: true, 
+        dataPoints: [
+          { x: 19, y: 12 },
+          { x: 27, y: 14 }
+        ]
+      }]
+    });
+    chart.render();
+    
+  }
+
+  flushStepObjects(){
+    
+  
+    this.stepObj2 = {
+      'distMatrix' :  [[{},{},{},{}],
+                      [{},{},{},{}],
+                      [{},{},{},{}],
+                      [{},{},{},{}]],
+      'minI' : 0,
+      'minJ' : 0,
+      'minVal' : Number.MAX_VALUE
+  
+    };
+  
+    this.stepObj3 = {
+      'distMatrix' :  [[{},{},{}],
+                      [{},{},{}],
+                      [{},{},{}]],
+      'minI' : 0,
+      'minJ' : 0,
+      'minVal' : Number.MAX_VALUE
+  
+    };
+  
+    this.stepObj4 = {
+      'distMatrix' :  [[{},{}],
+                      [{},{}]],
+      'minI' : 0,
+      'minJ' : 0,
+      'minVal' : Number.MAX_VALUE
+  
+    };
+  
+    this.titles = [];
   }
 
   constructor() { }
