14646499e390e49eafd42a9c62521508f4afe910
IgorPinchuk <igor_pig@mail.ru>
2019-10-21 13:30:23 +0300

    Всё

--- src/app/nearest-neighbor/nearest-neighbor.component.ts
+++ src/app/nearest-neighbor/nearest-neighbor.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, SystemJsNgModuleLoader } from '@angular/core';
 import * as CanvasJS from './canvasjs.min';
 
 @Component({
@@ -7,13 +7,13 @@ import * as CanvasJS from './canvasjs.min';
   styleUrls: ['./nearest-neighbor.component.css']
 })
 export class NearestNeighborComponent implements OnInit {
-
-  // x1a1 = 2.8; x1a2= 10;
-  // x2a1 = 4; x2a2= 7.8;
-  // x3a1 = 8.8; x3a2= 6;
-  // x4a1 = 12; x4a2= 11.8;
-  // x5a1 = 13.8; x5a2= 9;
-
+/*
+   x1a1 = 2.8; x1a2= 10;
+   x2a1 = 4; x2a2= 7.8;
+   x3a1 = 8.8; x3a2= 6;
+   x4a1 = 12; x4a2= 11.8;
+   x5a1 = 13.8; x5a2= 9;
+*/
   // x1a1 = 21; x1a2= 10;
   // x2a1 = 23; x2a2= 13;
   // x3a1 = 5; x3a2= 12;
@@ -25,6 +25,9 @@ export class NearestNeighborComponent implements OnInit {
   x3a1 = 8; x3a2= 6;
   x4a1 = 12; x4a2= 11;
   x5a1 = 13; x5a2= 9;
+
+  clusters = [];
+
 
   stepObj1 = {
     'distMatrix' : [[],[],[],[],[]],
@@ -61,7 +64,8 @@ export class NearestNeighborComponent implements OnInit {
                     [{},{}]],
     'minI' : 0,
     'minJ' : 0,
-    'minVal' : Number.MAX_VALUE
+    'minVal' : Number.MAX_VALUE,
+    'title' : []
 
   };
 
@@ -107,6 +111,8 @@ export class NearestNeighborComponent implements OnInit {
     this.addClusterToMatrix(this.stepObj1, this.stepObj2);
     this.addClusterToMatrix(this.stepObj2, this.stepObj3);
     this.addClusterToMatrix(this.stepObj3, this.stepObj4);
+    this.prepareFinalCluster();
+    console.log(this.clusters);
     this.renderChart();
   }
 
@@ -187,7 +193,78 @@ export class NearestNeighborComponent implements OnInit {
     curStep.title.push('('+prevStep.title[prevStep.minI]+', '+prevStep.title[prevStep.minJ]+')')
   }
 
+  prepareFinalCluster(){
+    let srcData = [
+      [this.x1a1.valueOf(), this.x1a2.valueOf()],
+      [this.x2a1.valueOf(), this.x2a2.valueOf()],
+      [this.x3a1.valueOf(), this.x3a2.valueOf()],
+      [this.x4a1.valueOf(), this.x4a2.valueOf()],
+      [this.x5a1.valueOf(), this.x5a2.valueOf()]
+    ];
+
+    let r = /\d+/g;
+    let m;
+    for(let i = 0; i< this.stepObj4.title.length; i++ ){
+      let arr =[];
+      while ((m = r.exec(this.stepObj4.title[i])) != null) {
+        arr.push(m[0]);
+      }
+      this.clusters.push({'title':this.stepObj4.title[i],'members': arr});
+    }
+    //Центр тяжести
+    this.clusters.forEach(element => {
+      let avg1 = 0;
+      let avg2 = 0;
+      element.members.forEach(element => {
+        avg1 += srcData[element-1][0];
+        avg2 += srcData[element-1][1];
+      });
+      avg1/=element.members.length;
+      avg2/=element.members.length;
+      element.center = [avg1, avg2]
+      element.avg1 = avg1;
+      element.avg2 = avg2;
+    });
+
+    //Дисперсия
+    this.clusters.forEach(element => {
+      let disper = 0;
+      let avg1 = element.avg1;
+      let avg2 = element.avg2;
+      element.members.forEach(element => {
+        disper+= Math.pow(srcData[element-1][0]-avg1,2);
+        disper+= Math.pow(srcData[element-1][1]-avg2,2);
+      });
+      disper/=element.members.length;
+      element.disper = disper;
+      element.avgSq = Math.pow(disper,0.5);
+    });
+
+    //Радиус
+    this.clusters.forEach(element => {
+      let radius = 0;
+      let maxRadius = 0;
+      let avg1 = element.avg1;
+      let avg2 = element.avg2;
+      element.members.forEach(element => {
+        radius = Math.pow(Math.pow(srcData[element-1][0]-avg1,2) + Math.pow(srcData[element-1][1]-avg2,2),0.5);
+        if(radius > maxRadius){
+          maxRadius = radius;
+        }
+      });
+      element.radius = maxRadius;
+    });
+
+  }
+
+
   renderChart(){
+
+    let centers =[];
+    this.clusters.forEach(element => {
+      centers.push({'x': element.center[0], 'y':element.center[1]})
+    });
+
     let chart = new CanvasJS.Chart("chartContainer", {
 
       data: [{
@@ -207,10 +284,7 @@ export class NearestNeighborComponent implements OnInit {
         type: "scatter",
         name: "Центры кластеров",
         showInLegend: true, 
-        dataPoints: [
-          { x: 19, y: 12 },
-          { x: 27, y: 14 }
-        ]
+        dataPoints: centers
       }]
     });
     chart.render();
@@ -246,11 +320,14 @@ export class NearestNeighborComponent implements OnInit {
                       [{},{}]],
       'minI' : 0,
       'minJ' : 0,
-      'minVal' : Number.MAX_VALUE
+      'minVal' : Number.MAX_VALUE,
+      'title' : []
   
     };
   
     this.titles = [];
+
+    this.clusters = [];
   }
 
   constructor() { }
